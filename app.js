97b2495f331ee287e6a1316e19b01cb50869c53a
BhardwajAbhay <abhaybhardwaj15704@gmail.com>
2025-07-15 10:17:44 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,95 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const { listingSchema, reviewSchema } = require("./schema.js");
+const Review = require("./models/review.js");
+const session = require("express-session");
+const flash = require("connect-flash");
+
+const listing = require("./routes/listing.js");
+const review = require("./routes/review.js");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public/css")));
+
+const sessionOption = {
+  secret: "mysupersecretcode",
+  resave: false,
+  saveUnitialized: true,
+  Cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  },
+};
+
+app.use(session(sessionOption));
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
+});
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+app.get("/", (req, res) => {
+  res.send("Hi, I am root");
+});
+
+// app.get("/testListing", async (req, res) => {
+//   let sampleListing = new Listing({
+//     title: "My New Villa",
+//     description: "By the beach",
+//     price: 1200,
+//     location: "Calangute, Goa",
+//     country: "India",
+//   });
+
+//   await sampleListing.save();
+//   console.log("sample was saved");
+//   res.send("successful testing");
+// });
+
+app.use("/listings", listing);
+app.use("/listings/:id/reviews", review);
+
+app.listen(8080, () => {
+  console.log("server is listening to port 8080");
+});
+
+//Catch-all for unmatched routes
+app.all(/.*/, (req, res, next) => {
+  next(new ExpressError(404, "Page Not Found!"));
+});
+
+// Error handler
+app.use((err, req, res, next) => {
+  console.error(err);
+  const { statusCode = 500, message = "Something went wrong!" } = err;
+  res.status(statusCode).render("error.ejs", { message });
+  //res.status(statusCode).send(message);
+});
