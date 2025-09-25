41da9503c2520f0493ac9a54ce6b40ad8a435c46
BhardwajAbhay <abhaybhardwaj15704@gmail.com>
2025-09-25 20:36:07 +0530

    today I add the ERROR handler

--- app.js
+++ app.js
@@ -116,24 +116,3 @@ app.use((err, req, res, next) => {
   //res.status(statusCode).send(message);
 });
 
-// Error handler
-app.use((err, req, res, next) => {
-  console.error(err);
-  const { statusCode = 500, message = "Something went wrong!" } = err;
-  res.status(statusCode).render("error.ejs", { message });
-  //res.status(statusCode).send(message);
-}); // Error handler
-app.use((err, req, res, next) => {
-  console.error(err);
-  const { statusCode = 500, message = "Something went wrong!" } = err;
-  res.status(statusCode).render("error.ejs", { message });
-  //res.status(statusCode).send(message);
-});
-
-// Error handler
-app.use((err, req, res, next) => {
-  console.error(err);
-  const { statusCode = 500, message = "Something went wrong!" } = err;
-  res.status(statusCode).render("error.ejs", { message });
-  //res.status(statusCode).send(message);
-});
