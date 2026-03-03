fe6279201957388784406b11a107e92eeeaff8d9
Abhay Bhardwaj <abhaybhardwaj15704@gmail.com>
2026-03-03 22:12:40 +0530

    Update project

--- models/user.js
+++ models/user.js
@@ -14,3 +14,6 @@ const userSchema = new Schema({
 userSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model("User", userSchema);
+
+
+module.exports = mongoose.model("User", userSchema);
